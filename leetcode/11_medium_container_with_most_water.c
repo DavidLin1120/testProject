646524d1ff117ea91bd2ba96ae5143583a4b3f64
DavidLin1120 <32158362+DavidLin1120@users.noreply.github.com>
2021-12-05 00:55:50 +0800

    11. Container With Most Water V2 Runtime: 80 ms, faster than 57.77% of C online submissions for Container With Most Water. Memory Usage: 11.7 MB, less than 61.02% of C online submissions for Container With Most Water.

--- leetcode/11_medium_container_with_most_water.c
+++ leetcode/11_medium_container_with_most_water.c
@@ -1,38 +1,57 @@
-
-
 int maxArea(int* height, int heightSize){
-    int i = 0 , left = 0 , right=0;;
-    int max = 0;
-    int len = 0;
-    int found_len=-1;
+    int max=0, left=0, right=heightSize-1;
+    int short_height=0;
+    int tmp_area=0;
     if(heightSize <2)
-        return 0;
-    
-    for ( i = 0 ; i < heightSize ; i ++){
-        found_len=-1;
-        for (left = 0 ; left < i ; left++){
-            if(!(height[left] < height[i])){
-                if(max < height[i] * (i-left)){
-                    max = height[i]*(i-left);
-                }
-                found_len = (i-left);
-                break;
-            }
-        }
-        if (found_len > -1){
-            if (heightSize -1 - i < found_len)
-                continue;
-        }
-        for (right = heightSize-1 ; right> i ; right--){
-             if(!(height[right] < height[i])){
-                if(max < height[i] * (right-i)){
-                    max = height[i]*(right-i);
-                }
-                 break;
-            }
-            
-        }
+         return 0;
+    while(left < right){
+        short_height = height[left] < height[right] ? height[left] : height[right]; 
+        
+        tmp_area = short_height * (right-left);
+        
+        max = max < tmp_area ? tmp_area : max;
 
+        height[left] < height[right] ? left++ : right--;
+
+        
     }
     return max;
+    
 }
+
+// int maxArea(int* height, int heightSize){
+//     int i = 0 , left = 0 , right=0;;
+//     int max = 0;
+//     int len = 0;
+//     int found_len=-1;
+//     if(heightSize <2)
+//         return 0;
+    
+//     for ( i = 0 ; i < heightSize ; i ++){
+//         found_len=-1;
+//         for (left = 0 ; left < i ; left++){
+//             if(!(height[left] < height[i])){
+//                 if(max < height[i] * (i-left)){
+//                     max = height[i]*(i-left);
+//                 }
+//                 found_len = (i-left);
+//                 break;
+//             }
+//         }
+//         if (found_len > -1){
+//             if (heightSize -1 - i < found_len)
+//                 continue;
+//         }
+//         for (right = heightSize-1 ; right> i ; right--){
+//              if(!(height[right] < height[i])){
+//                 if(max < height[i] * (right-i)){
+//                     max = height[i]*(right-i);
+//                 }
+//                  break;
+//             }
+            
+//         }
+
+//     }
+//     return max;
+// }
